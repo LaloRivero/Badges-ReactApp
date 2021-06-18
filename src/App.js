8d043ae32283e8681ceb0758b74bebde3cda276b
Eduardo Rivero <lalo@lalorivero.com>
2021-06-18 13:21:23 -0600

    bug fixing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-//import Landing from "../src/pages/Landing/Landing";
+
+import Landing from "../src/pages/Landing/Landing";
 import Layout from "../src/pages/Layout/Layout";
 import NewBadge from "../src/pages/NewBadge/NewBadge";
 import Badges from "../src/pages/Badges/Badges";
@@ -8,12 +9,13 @@ import BadgeDetails from "../src/components/BadgeDetails";
 import NotFound from "../src/pages/NotFound/NotFound";
 import EditBadge from "../src/pages/EditBadge/EditBadge";
 
+
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Switch>
-          {/* <Route exact path="/" component={Landing}></Route> */}
+          <Route exact path="/" component={Landing}></Route>
           <Route exact path="/new" component={NewBadge}></Route>
           <Route exact path="/badges" component={Badges}></Route>
           <Route exact path="/:badgeId" component={BadgeDetails}></Route>
